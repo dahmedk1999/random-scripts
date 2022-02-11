e66bfa06a41687cc865b2da2849e9a6fa560b66d
dahmedk1999 <danish.khan@sjsu.edu>
2022-02-10 18:28:03 -0800

    unordered_map imp

--- Submissions_Leetcode/SubArraySumEqualsK.cpp
+++ Submissions_Leetcode/SubArraySumEqualsK.cpp
@@ -2,7 +2,8 @@
 using std::vector;
 #include <iostream>
 using std::cout;
-
+#include <unordered_map>
+using std::unordered_map;
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) 
@@ -25,11 +26,29 @@ public:
         return ret;
     }
 };
-
+class Solution2 {
+public:
+    int subarraySum(vector<int>& nums, int k) 
+    {
+        int sum=0,count=0;
+        std::unordered_map<int,int> map={{0,1}};
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            count+=map[sum-k];
+            map[sum]++;
+        }
+        return count;
+    }
+};
 
 int main()
 {
     Solution test;
     vector<int> v = {1,2,3,4,5,-6,6,6,7};
     cout<<test.subarraySum(v,5);
+
+    Solution2 test2;
+    cout<<test2.subarraySum(v,5);
+
 }
